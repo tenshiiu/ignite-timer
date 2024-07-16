d423f908fe76da646082dae62cd0c5fbd24eca42
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-07-16 11:42:08 -0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,15 @@
+import { Button } from './components/Button'
+
+export function App() {
+  
+  return (
+    <>
+      <Button variant='primary'/>
+      <Button variant='secondary'/>
+      <Button variant='danger'/>
+      <Button variant='success'/>
+    </>
+  )
+}
+
+
