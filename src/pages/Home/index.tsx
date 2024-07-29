c6fc85d741cbe3fb17c65ed19cdc759436be892e
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-07-29 15:51:48 -0300

    Correção de bugs e timer no title

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -15,7 +15,6 @@ const newCycleFormValidationSchema = zod.object({
   .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
 })
 
-
 type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
 
 interface Cycle {
@@ -41,11 +40,16 @@ export function Home() {
   const activeCycle = cycles.find(cycle => cycle.id == activeCycleId);
 
   useEffect(() => {
+    let interval: number;
+
     if (activeCycle) {
-      setInterval(() => {
+      const interval = setInterval(() => {
         setamountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate),
        )
       }, 1000)
+    }
+    return () => {
+      clearInterval(interval);
     }
   }, [activeCycle])
 
@@ -61,6 +65,7 @@ export function Home() {
     
     setCycles((state) => [...state, newCycle]);
     setActiveCycleId(id);
+    setamountSecondsPassed(0);
 
     reset();
   }
@@ -74,6 +79,12 @@ export function Home() {
 
   const minutes = String(minutesAmount).padStart(2, '0');
   const seconds = String(secondsAmount).padStart(2, '0');
+
+  useEffect(() => {
+    if (activeCycle) {
+      document.title = `${minutes}:${seconds}`
+    }
+  }, [minutes, seconds, activeCycle])
 
   const task = watch('task');
   const isSubmitDisabled = !task;
