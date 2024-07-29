647f1066546b80387f6052c3d58a8199e7383835
Raul Vinícius <165701907+tenshiiu@users.noreply.github.com>
2024-07-29 15:01:57 -0300

    Tornando o cronômetro funcional

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -3,6 +3,9 @@ import { CountdownContainer, FormContainer, HomeContainer, Separator, StartCount
 import { useForm } from "react-hook-form";
 import { zodResolver } from "@hookform/resolvers/zod";
 import * as zod from "zod";
+import { differenceInSeconds } from 'date-fns';
+
+import { useEffect, useState } from "react";
 
 const newCycleFormValidationSchema = zod.object({
   task: zod.string().min(1, "informe a tarefa"),
@@ -12,12 +15,20 @@ const newCycleFormValidationSchema = zod.object({
   .max(60, "O ciclo precisa ser de no máximo 60 minutos"),
 })
 
-interface NewCycleFormData {
+
+type NewCycleFormData = zod.infer<typeof newCycleFormValidationSchema>
+
+interface Cycle {
+  id: string,
   task: string,
-  MinutesAmount: number;
+  minutesAmount: number,
+  startDate: Date,
 }
 
 export function Home() {
+  const [ cycles, setCycles ] = useState<Cycle[]>([]);
+  const [ activeCycleId, setActiveCycleId ] = useState<string | null>(null);
+  const [ amountSecondsPassed, setamountSecondsPassed ] = useState(0);
 
   const { register, handleSubmit, watch, reset } = useForm({
     resolver: zodResolver(newCycleFormValidationSchema),
@@ -27,10 +38,42 @@ export function Home() {
     }
   });
 
+  const activeCycle = cycles.find(cycle => cycle.id == activeCycleId);
+
+  useEffect(() => {
+    if (activeCycle) {
+      setInterval(() => {
+        setamountSecondsPassed(differenceInSeconds(new Date(), activeCycle.startDate),
+       )
+      }, 1000)
+    }
+  }, [activeCycle])
+
   function handleCreateNewCycle(data: NewCycleFormData) {
-    console.log(data);
+    const id = String(new Date().getTime());
+
+    const newCycle: Cycle = {
+      id,
+      task: data.task,
+      minutesAmount: data.minutesAmount,
+      startDate: new Date(),
+    };
+    
+    setCycles((state) => [...state, newCycle]);
+    setActiveCycleId(id);
+
     reset();
   }
+
+
+  const totalSeconds = activeCycle ? activeCycle.minutesAmount * 60 : 0;
+  const currentSeconds = activeCycle ? totalSeconds - amountSecondsPassed : 0;
+
+  const minutesAmount = Math.floor(currentSeconds / 60);
+  const secondsAmount = currentSeconds % 60;
+
+  const minutes = String(minutesAmount).padStart(2, '0');
+  const seconds = String(secondsAmount).padStart(2, '0');
 
   const task = watch('task');
   const isSubmitDisabled = !task;
@@ -65,7 +108,7 @@ export function Home() {
         step={5} 
         min={5} 
         max={60}
-         {...register("minutesAmount", {valueAsNumber: true})}
+         {...register('minutesAmount', {valueAsNumber: true})}
         />
 
         <span>minutos.</span>
@@ -73,11 +116,11 @@ export function Home() {
       
 
       <CountdownContainer >
-        <span>0</span>
-        <span>0</span>
+        <span>{minutes[0]}</span>
+        <span>{minutes[1]}</span>
         <Separator>:</Separator>
-        <span>0</span>
-        <span>0</span>
+        <span>{seconds[0]}</span>
+        <span>{seconds[1]}</span>
       </CountdownContainer >
 
        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
